50bc55c0610eda6c0660077050337fecd709c35e
annavaleriev <filenko.a@gmail.com>
2023-10-01 14:13:01 +0300

    сделаны тесты для def  calculate_total_price и def apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def class_test_item():
+    return Item("Смартфон", 10000, 2)
+
+
+def test_calculate_total_price(class_test_item):
+    assert class_test_item.calculate_total_price() == 20000
+
+
+def test_apply_discount(class_test_item):
+    assert class_test_item.apply_discount() is None
