fcc4507a639b7954885fbe1b47d27e957bbfed43
annavaleriev <filenko.a@gmail.com>
2023-11-30 23:36:17 +0300

    обновлен метод def instantiate_from_csv, добавлены исключения

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,11 @@
 from src.item import Item
+from src.settings import BROKEN_ITEMS_CSV_PATH
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv("kkkkkk")
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(BROKEN_ITEMS_CSV_PATH)
     # InstantiateCSVError: Файл item.csv поврежден
---
+++ src/settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+ITEMS_CSV_PATH = Path.joinpath(ROOT_PATH, 'items.csv')
+
+BROKEN_ITEMS_CSV_PATH = Path.joinpath(ROOT_PATH, 'items_1.csv')
